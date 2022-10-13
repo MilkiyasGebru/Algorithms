ce738de8676b53660c593e1df4bafa4d2af67ae4
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-13 09:02:44 +0300

    Time: 3035 ms (36.96%), Space: 43.9 MB (64.56%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,16 +1,16 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         child = defaultdict(list)
-        
+        ans = 0
         for i in range(len(manager)):
             child[manager[i]].append(i)
-        
-        def dfs(node):
-            if len(child[node]) == 0:
-                return 0
-            y = informTime[node] 
-            x=-inf
+        q=deque()
+        q.append((headID,0))
+        while q:
+            
+            node,time = q.popleft()
+            ans = max(ans,time)
+            
             for c in child[node]:
-                x = max(dfs(c),x)
-            return x+y
-        return dfs(headID)
+                q.append((c,time+informTime[node]))
+        return ans
