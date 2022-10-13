19bf8a18794cf35f6d618b2698ade6f83f299df1
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-13 10:49:36 +0300

    Time: 1443 ms (82.31%), Space: 50.7 MB (34.30%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,18 +1,21 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        
         n = len(edges)
         visited = set()
-        arr = [0]*n
-        arr2 = [-inf]*n
         
-        graph = defaultdict(list)
-        for i in range(len(edges)):
-            if edges[i] != -1:
-                graph[i].append(edges[i])
+        arr = [0]*n
+        arr2 = [-inf]*n 
+        
         q=deque()
+        
         q.append((node1,0,0))
         q.append((node2,1,0))
+        
+        ans = [inf,0]
+        
         while(q):
+            
             node,prev,dis=q.popleft()
             
             if (node,prev) in visited:
@@ -22,13 +25,15 @@ class Solution:
             visited.add((node,prev))
             arr[node]+=1
             arr2[node]=max(dis,arr2[node])
-            for c in graph[node]:
-                q.append((c,prev,dis+1))
-        ans = [inf,0]
-        for i in range(len(arr2)):
-            if arr2[i]==-inf or arr[i]!=2:
-                continue
-            if ans[0] > arr2[i]:
-                ans = [arr2[i],i]
+            
+            if arr[node]==2:
+                
+                if ans[0] > arr2[node] or (ans[0]== arr2[node] and node < ans[1]):
+                    ans = [arr2[node],node]
+                    
+            if edges[node]!=-1:
+                q.append((edges[node],prev,dis+1))
+        
+        
         return ans[1] if ans[0]!= inf else -1
             
