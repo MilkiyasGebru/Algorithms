7b08280e6d820b5115c62605e5cbb2f5410b2daa
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-18 15:28:18 +0300

    Time: 109 ms (22.51%), Space: 14.3 MB (7.99%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,19 +2,26 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
+        
         x = self.countAndSay(n-1)
         s = []
-        prev = ""
-        count = 0
-        for i in range(len(x)):
+        
+        prev = x[0]
+        count = 1
+        
+        for i in range(1,len(x)):
+            
             if x[i]!=prev :
-                if prev != "":
-                    s.append(str(count))
-                    s.append(prev)
+                
+                s.append(str(count))
+                s.append(prev)
+                
                 prev = x[i]
                 count = 1
+                
             else:
                 count+=1
+                
         s.append(str(count))
         s.append(prev)
         
