36af795b9a7fec5256b09d5a5d6817c4a8e08d20
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-20 14:14:58 +0300

    Time: 1228 ms (12.88%), Space: 22.8 MB (5.09%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -3,9 +3,12 @@ class Solution:
         
         dp = defaultdict(lambda:[0,0])
         max_area = 0
+        
         for row in range(len(matrix)-1,-1,-1):
             depth = {}
+            
             for col in range(len(matrix[0])-1,-1,-1):
+                
                 if matrix[row][col]=="1":
                     
                     dp[(row,col)][0]=1+dp[(row,col+1)][0]
@@ -13,6 +16,7 @@ class Solution:
                     
                     if dp[(row,col)][1] not in depth: 
                         depth[dp[(row,col)][1]] = col + 1
+                        
                     deleted = []
                     for key in depth.keys():
                         if key > dp[(row,col)][1]:
@@ -21,9 +25,7 @@ class Solution:
                     for key in deleted:
                         del depth[key]
                             
-                            
-                    
-                        
+                   
                     for dep in depth.keys():
                         
                         max_area = max(dep*(depth[dep]-col),max_area)
@@ -31,5 +33,6 @@ class Solution:
                 else:
                     
                     depth = {}
+                    
         return max_area
                 
