6156bd507086eef17215169dd6876947db77ca76
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-21 09:40:07 +0300

    Time: 529 ms (83.68%), Space: 66.6 MB (5.12%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -0,0 +1,48 @@
+class RandomizedCollection:
+
+    def __init__(self):
+        
+        self.numbers = []
+        self.position = defaultdict(set)
+
+    def insert(self, val: int) -> bool:
+        
+        isNotFound = len(self.position[val]) == 0
+        
+        self.numbers.append(val)
+        self.position[val].add(len(self.numbers)-1)
+        
+        return isNotFound
+        
+
+    def remove(self, val: int) -> bool:
+        
+        isFound = len(self.position[val]) > 0
+        
+        if isFound :
+            
+            for index in self.position[val]:
+                break
+                
+            self.position[val].remove(index)
+            if index != len(self.numbers)-1:
+                self.position[self.numbers[-1]].remove(len(self.numbers)-1)
+            self.numbers[-1],self.numbers[index]=self.numbers[index],self.numbers[-1]
+            self.numbers.pop()
+            
+            if index < len(self.numbers):
+                self.position[self.numbers[index]].add(index)
+            return True
+            
+        
+
+    def getRandom(self) -> int:
+        
+        return random.choice(self.numbers)
+
+
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
