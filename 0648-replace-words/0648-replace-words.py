3ab55f5feb308f953768fc79e1fe98a2e7bca9c9
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-25 09:41:47 +0300

    Time: 375 ms (49.57%), Space: 35.9 MB (25.14%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,41 +1,49 @@
 class TrieNode:
     def __init__(self):
-        self.children = defaultdict(TrieNode)
+        
+        self.children = {}
         self.isEnd = False
+        
 class Trie:
     
     def __init__(self):
+        
         self.root = TrieNode()
     
     def addWord(self,word):
+        
         node = self.root
         
         for letter in word:
+            
             if node.isEnd :
                 return
-            if node.children[letter] :
-                node = node.children[letter]
-            else:
+            if letter not in node.children :
                 node.children[letter]=TrieNode()
-                node =  node.children[letter]
+            node =  node.children[letter]
+            
         node.isEnd = True
         
     def findWord(self,word):
+        
         successor = []
         node = self.root
+        
         for letter in word:
-            if node.children[letter] :
+            
+            if letter in node.children :
+                
                 successor.append(letter)
                 node = node.children[letter]
-                if node.isEnd:
-                    return "".join(successor)
-            else:
-                return word
-        if node.isEnd:
-            return "".join(successor)
-        return word
                 
+                if node.isEnd:
+                    break
+            else:
+                
+                break
         
+        return "".join(successor) if node.isEnd else word
+            
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
@@ -43,6 +51,7 @@ class Solution:
         
         trie = Trie()
         answer = []
+        
         for word in dictionary:
             trie.addWord(word)
         
