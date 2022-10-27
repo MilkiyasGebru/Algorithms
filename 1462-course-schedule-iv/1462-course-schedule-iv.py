98a7cbf9e607de7a35e1063d02ebfd75b8155c8d
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-27 11:48:18 +0300

    Time: 7386 ms (5.03%), Space: 17.7 MB (64.11%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,41 +1,18 @@
 class Solution:
-    def addParent(self,node,par,graph,parent):
-        
-        if par in parent[node]:
-            return 
-        
-        parent[node].add(par)
-        
-        for child in graph[node]:
-            self.addParent(child,par,graph,parent)
-            
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
         
-        indegree = [0  for _ in range(numCourses)]
-        parent = defaultdict(set)
-        graph = defaultdict(set)
-        
-        queue = deque()
+        dp=[[False for _ in range(numCourses)] for _ in range(numCourses)]
         
         for pre,after in prerequisites:
-            graph[pre].add(after)
-            indegree[after]+=1
+            dp[pre][after] = True
             
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
-                queue.append(i)
-        
-        while(queue):
-            node = queue.popleft()
-            for child in graph[node]:
-                self.addParent(child,node,graph,parent)
-                indegree[child]-=1
-                if indegree[child]==0:
-                    queue.append(child)
-        answer = []
-        for pre,after in queries:
-            answer.append(pre in parent[after])
-        return answer
-        
-        
-        
+        for k in range(numCourses):
+            
+            for i in range(numCourses):
+                
+                for j in range(numCourses):
+                    
+                    dp[i][j] = dp[i][j] or  (dp[i][k] and dp[k][j])
+                              
+        return [dp[pre][after] for pre,after in queries]
+                    
