d1c928e06d1a2c1ba872ebf77f7cea6e72f63fc5
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-10-31 09:11:40 +0300

    Time: 91 ms (92.91%), Space: 14 MB (6.96%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,16 +1,17 @@
 class Solution:
     
+    def isValid(self,row,col,m,n):
+        
+        return 0 <= row < m and 0 <= col < n
     
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         
-        elements = {}
         
-        for row in range(len(matrix)):
+        for row in range(1,len(matrix)):
             
             for col in range(len(matrix[0])):
                 
-                if (row-col) not in elements:
-                    elements[row-col]=matrix[row][col]
-                elif matrix[row][col]!=elements[row-col]:
+                if self.isValid(row-1,col-1,len(matrix),len(matrix[0])) and matrix[row][col]!=matrix[row-1][col-1]:
                     return False
+                
         return True
