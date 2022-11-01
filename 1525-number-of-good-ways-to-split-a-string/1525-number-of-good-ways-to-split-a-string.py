855b1a0ec94f10fa7f1a5fdcac47b1c76e015c72
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-11-01 11:02:09 +0300

    Time: 465 ms (40.69%), Space: 14.6 MB (67.21%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.py
@@ -1,19 +1,29 @@
 class Solution:
     def numSplits(self, s: str) -> int:
         
-        forward_letters = []
-        backward_letters = []
+        forward_letters = set()
+        backward_letters = {}
         
-        visited = set()
-        for i in s:
-            visited.add(i)
-            forward_letters.append(len(visited))
-        visited = set()
+        
         for i in range(len(s)-1,-1,-1):
-            visited.add(s[i])
-            backward_letters.append(len(visited))
+            
+            if s[i] not in backward_letters:
+                backward_letters[s[i]] = 0
+                
+            backward_letters[s[i]]+=1
+                
         good_splits = 0
-        for left in range(len(s)-1):
-            if forward_letters[left] == backward_letters[len(s)-left-2]:
+        
+        for i in range(len(s)):
+            
+            forward_letters.add(s[i])
+            backward_letters[s[i]]-=1
+            
+            if backward_letters[s[i]]==0:
+                del backward_letters[s[i]]
+                
+            if len(forward_letters) == len(backward_letters):
                 good_splits += 1
+            
+            
         return good_splits
