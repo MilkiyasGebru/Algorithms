4b517688588f0b55ff12cd5917a0533762fb52f3
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-11-01 10:02:36 +0300

    Time: 228 ms (84.76%), Space: 14.4 MB (30.84%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -13,23 +13,32 @@ class Solution:
         for row in range(len(grid)):
 
             for col in range(len(grid[0])):
+                
                 if falls_array[col] == False:
                     continue
+                    
                 r = falls_array[col][0]
                 c = falls_array[col][1]
+                
                 if self.check(r,c+ grid[r][c],len(grid),len(grid[0])) and grid[r][c] == grid[r][c + grid[r][c]]:
                     falls_array[col] = [r+1,c+grid[r][c]]
+                    
                 else:
+                    
                     falls_array[col]=False
-        
-                
                 
         answer = []
+        
         for col in range(len(falls_array)):
+            
             if falls_array[col]!=False:
+                
                 answer.append(falls_array[col][1])
+                
             else:
+                
                 answer.append(-1)
+                
         return answer
                     
                 
