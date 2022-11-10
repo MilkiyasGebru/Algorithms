532e413a49d9420f556c8c8631d8d121337389bb
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2022-11-10 09:01:48 +0300

    Time: 656 ms (98.87%), Space: 21.5 MB (5.15%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,11 +5,15 @@ class Solution:
             return -1
         
         prefix = []
+        
         for i in range(len(gas)):
+            
             if len(prefix) == 0:
                 prefix.append(gas[i]-cost[i])
             else:
                 prefix.append(prefix[-1] + gas[i] - cost[i])
-        prefix = prefix[::-1]
+                
+        prefix.reverse()
         last_index =len(cost) - 1 -  prefix.index(min(prefix))
+        
         return (last_index+1)%(len(prefix))
