efeea63a57f2a14bd2e4f3f6869f14c179799c87
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-01-29 16:32:16 +0300

    Time: 108 ms (63.37%), Space: 17.6 MB (9.70%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        color = [0]*numCourses
-        graph=defaultdict(list)
         
-        for nextCourse,prevCourse in prerequisites:
-            graph[prevCourse].append(nextCourse)
-            
-        ans = []
-        def dfs(currentCourse):
-            
-            if color[currentCourse]==1:
-                return True
-            if color[currentCourse]==2:
-                return False
-            color[currentCourse]=1
-            
-            for nextCourse in graph[currentCourse]:
-                if  dfs(nextCourse):
-                    return True
-            color[currentCourse]= 2
-            ans.append(currentCourse)
-        for i in range(numCourses):
-            if dfs(i):
-                return []
-        return ans[::-1]
-            
-            
-            
+        colors = [0 for _ in range(numCourses)]
+        graph = defaultdict(list)
         
+        for course1,course2 in prerequisites:
+            graph[course2].append(course1)
+        
+        answer = []
+        
+        
+        def dfs(course):
+            
+            colors[course] = 1
+            
+            for neigbour in graph[course]:
+                
+                if colors[neigbour] == 0:
+                    dfs(neigbour)
+                elif colors[neigbour] == 1:
+                    return
+            
+            colors[course] = 2
+            answer.append(course)
+        
+        for course in range(numCourses):
+            if colors[course] == 0:
+                dfs(course)
+        return answer[::-1] if len(answer) == numCourses else []
