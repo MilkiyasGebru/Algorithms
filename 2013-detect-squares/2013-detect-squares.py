006777cedf58958cf30aaa4cbe2ab540120d4908
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-02-06 19:06:47 +0300

    Time: 7997 ms (5.08%), Space: 19.9 MB (8.02%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.py
@@ -0,0 +1,32 @@
+class DetectSquares:
+
+    def __init__(self):
+        
+        self.points = defaultdict(int)
+
+    def add(self, point: List[int]) -> None:
+        
+        self.points[(point[0],point[1])] += 1
+
+    def count(self, point: List[int]) -> int:
+        answer = 0
+        keys = list(self.points.keys())
+        for key in keys:
+            if key[0] == point[0] and key[1] == point[1]:
+                continue
+            if key[0] == point[0]:
+                diff = abs(key[1] - point[1])
+                answer += self.points[key] * (self.points[(key[0] + diff, key[1])] * self.points[(point[0] + diff, point[1])] + self.points[(key[0] - diff, key[1])] * self.points[(point[0] - diff, point[1])])
+            elif key[1] == point[1]:
+                diff = abs(key[0] - point[0])
+                answer += self.points[key] * (self.points[(key[0] , key[1] + diff)] * self.points[(point[0] , point[1] + diff)] + self.points[(key[0] , key[1] - diff)] * self.points[(point[0] , point[1]- diff)])
+        return answer//2 
+    
+    
+        
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
