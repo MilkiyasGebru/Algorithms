92dfbf293aa523bf0ca787f03adb3cb3ca417b2c
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-02-06 19:11:03 +0300

    Time: 490 ms (49.44%), Space: 15.9 MB (60.61%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -15,12 +15,12 @@ class DetectSquares:
             if key[0] == point[0] and key[1] == point[1]:
                 continue
             if key[0] == point[0]:
+                
                 diff = abs(key[1] - point[1])
+                
                 answer += self.points[key] * (self.points[(key[0] + diff, key[1])] * self.points[(point[0] + diff, point[1])] + self.points[(key[0] - diff, key[1])] * self.points[(point[0] - diff, point[1])])
-            elif key[1] == point[1]:
-                diff = abs(key[0] - point[0])
-                answer += self.points[key] * (self.points[(key[0] , key[1] + diff)] * self.points[(point[0] , point[1] + diff)] + self.points[(key[0] , key[1] - diff)] * self.points[(point[0] , point[1]- diff)])
-        return answer//2 
+            
+        return answer
     
     
         
