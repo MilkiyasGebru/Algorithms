7f08f8508cd621aa481fae5358f6e58ed5afffd9
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-02-09 19:46:53 +0300

    Time: 42 ms (48.38%), Space: 14 MB (28.39%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        answer = set()
+        
+        def backTrack(index,path):
+            
+            if len(path) > 4:
+                return
+            
+            if index == len(s):
+                
+                if len(path) == 4:
+                    for i in range(4):
+                        path[i] = str(int(path[i]))
+                    answer.add(".".join(path))
+                return
+            if path != [] and (int(path[-1] + s[index]) > 255 or path[-1] == "0"):
+                backTrack(index+1,path + [s[index]])
+                return
+            backTrack(index+1,path + [s[index]])
+            if path:
+                poped = path.pop()
+                backTrack(index+1,path + [poped + s[index]])
+            else:
+                backTrack(index+1,[s[index]])
+        
+        backTrack(0,[])
+        return answer
+            
+            
