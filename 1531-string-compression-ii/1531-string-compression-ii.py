b25443758a78495d3ba7af858dbf9bd0f4ffd0f6
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-02-16 23:11:57 +0300

    Time: 2535 ms (37.44%), Space: 369.5 MB (49.74%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        
+        @lru_cache(None)
+        def dp(i,last_count,last_letter,k):
+            if k < 0:
+                return inf
+            if i == len(s):
+                if last_count == 0:
+                    return 0
+                return len(str(last_count)) + 1 if last_count != 1 else 1
+            if s[i] == last_letter:
+                
+                return min(
+                    dp(i+1,last_count+1,last_letter,k)
+                    ,dp(i+1,last_count,last_letter,k-1)
+                )
+            count =0
+            if last_count != 0:
+                count = 1 if last_count == 1 else len(str(last_count)) + 1
+            return min(
+                dp(i+1,last_count,last_letter,k-1)
+                ,dp(i+1,1,s[i],k) + count
+            )
+        
+        return dp(0,0,"",k)
+                
+        
+
+        
+                
+        
+                        
+            
