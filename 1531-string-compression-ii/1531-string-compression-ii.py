75e931b29b2704d223a613332d3136458df67099
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-02-16 23:13:45 +0300

    Time: 2532 ms (37.44%), Space: 369.4 MB (49.74%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -3,21 +3,25 @@ class Solution:
         
         @lru_cache(None)
         def dp(i,last_count,last_letter,k):
+            
+            count =0
+            
+            if last_count != 0:
+                count = 1 if last_count == 1 else len(str(last_count)) + 1
+                
             if k < 0:
                 return inf
-            if i == len(s):
-                if last_count == 0:
-                    return 0
-                return len(str(last_count)) + 1 if last_count != 1 else 1
+            
+            if i == len(s): 
+                return count
+            
             if s[i] == last_letter:
                 
                 return min(
                     dp(i+1,last_count+1,last_letter,k)
                     ,dp(i+1,last_count,last_letter,k-1)
                 )
-            count =0
-            if last_count != 0:
-                count = 1 if last_count == 1 else len(str(last_count)) + 1
+            
             return min(
                 dp(i+1,last_count,last_letter,k-1)
                 ,dp(i+1,1,s[i],k) + count
