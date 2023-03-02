c5be398a77eec6ec589e74ac24be49b6884b8e23
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-02 10:29:17 +0300

    Time: 2650 ms (18.41%), Space: 19.9 MB (50.85%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        
+        def check(s):
+        
+            return s == s[::-1]
+        
+        @lru_cache(None)
+        def partition( s: str) -> List[List[str]]:
+        
+            answer = len(s)
+
+
+
+            for i in range(len(s)):
+
+                if check(s[:i+1]):
+
+                    answer = min(1 + partition(s[i+1:]),answer)
+
+            return answer
+        
+        
+        
+        return partition(s) - 1
