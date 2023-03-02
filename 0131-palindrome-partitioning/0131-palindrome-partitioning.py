d2eecc1ca5a8e844e72e1d52d6d436ec3be098e7
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-02 10:20:27 +0300

    Time: 834 ms (40.14%), Space: 36.4 MB (9.57%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,5 +1,7 @@
 class Solution:
-    memo = {}
+    
+    
+    
     def check(self,s):
         
         return s == s[::-1]
@@ -7,8 +9,9 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
         answer = []
-        if s in self.memo:
-            return self.memo[s]
+        
+        
+        
         for i in range(len(s)):
             
             if self.check(s[:i+1]):
@@ -21,5 +24,4 @@ class Solution:
                 for palindrom in palindroms:
                     answer.append([s[:i+1]] + palindrom)
             
-        self.memo[s] = answer      
-        return self.memo[s]
+        return answer
