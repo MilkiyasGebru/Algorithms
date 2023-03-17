0b2be5d7dbfdc7ccef911f198e68a798b614e460
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-17 11:05:59 +0300

    Time: 1333 ms (45.63%), Space: 160 MB (9.51%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -4,22 +4,23 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
+    def rec(self,node):
+        
+        if not node.next:
+            
+            return node
+        
+        right_max = self.rec(node.next)
+        
+        if node.val >= right_max.val:
+            
+            node.next = right_max
+            return node
+        
+        return right_max
+    
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        
-        def rec(node):
-            
-            if not node.next:
-                return node
-            
-            right_max = rec(node.next)
-            
-            if node.val >= right_max.val:
-                
-                node.next = right_max
-                return node
-            
-            return right_max
-        
-        return rec(head)
+        return self.rec(head)
         
