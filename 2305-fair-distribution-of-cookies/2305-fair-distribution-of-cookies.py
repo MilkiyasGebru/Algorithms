dfdec1b691ad02663e8e7cc90c6e1efd4b524ee4
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-19 19:26:21 +0300

    Time: 960 ms (42.32%), Space: 14 MB (21.76%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,26 +1,27 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-
-        def binary_search(left: int, right: int, fn: callable) -> int:
-            while left < right:
-                mid = left + right >> 1
-                left, right = (mid + 1, right) if fn(mid) else (left, mid)
-            return left
-
-        def condition(limit: int) -> bool:
-
-            def dfs(i: int) -> bool:
-                if i == len(cookies):
-                    return True
-                for j in range(k):
-                    if bags[j] + cookies[i] <= limit:
-                        bags[j] += cookies[i]
-                        if dfs(i + 1):
-                            return True
-                        bags[j] -= cookies[i]
-                return False
-
-            bags = [0] * k
-            return not dfs(0)
-
-        return binary_search(max(cookies), sum(cookies), condition)
+        
+        
+        cookies.sort(reverse = True)
+        self.max = math.inf
+        arr = [0 for _ in range(k)]
+        
+        def backtrack(i):
+            
+            if i == len(cookies):
+                
+                self.max = min(self.max,max(arr))
+                return
+            
+            for j in range(k):
+                
+                if arr[j] + cookies[i] < self.max:
+                    
+                    arr[j] += cookies[i]
+                    backtrack(i+1)
+                    arr[j] -= cookies[i]
+                    
+        
+        backtrack(0)
+        return self.max
+            
