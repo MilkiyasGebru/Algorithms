aaab8c9289e70d3be8e034e693643ea7469d76e3
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-23 17:19:35 +0300

    Time: 512 ms (64.29%), Space: 48.4 MB (17.29%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,60 +1,44 @@
-class UnionFind:
+
     
-    def __init__(self,n):
         
-        self.parent = [ i for i in range(n)]
-        self.rank = [0 for _ in range(n)]
-    
-    def findParent(self,computer):
         
-        if self.parent[computer] == computer:
-            return computer
-        
-        self.parent[computer] = self.findParent(self.parent[computer])
-        return self.parent[computer]
-    
-    def union(self,u,v):
-        
-        parent1 = self.findParent(u)
-        parent2 = self.findParent(v)
-        
-        if parent1 == parent2:
-            return 1
-        
-        if self.rank[parent1] > self.rank[parent2]:
-            
-            self.parent[parent2] = parent1
-            self.rank[parent2] += self.rank[parent1]
-            
-        elif self.rank[parent1] < self.rank[parent2]:
-            
-            self.parent[parent1] = parent2
-            self.rank[parent1] += self.rank[parent2]
-        
-        else:
-            self.parent[parent1] = parent2
-            self.rank[parent1] += 1
-        
-        return 0
-    def total_parents(self):
-        parents = set()
-        for i in range(len(self.parent)):
-            parents.add(self.findParent(self.parent[i]))
-        
-        return len(parents)-1
         
 
 class Solution:
+    
+    def dfs(self,node,graph,visited):
+        
+        if node in visited:
+            return 
+        
+        visited.add(node)
+        
+        for neigbour in graph[node]:
+            self.dfs(neigbour,graph,visited)
+    
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         
-        redudant_cabels = 0
-        network = UnionFind(n)
+        if len(connections) < n - 1:
+            return -1
+        
+        visited = set()
+        graph = defaultdict(list)
+        components = 0
+        
         for u,v in connections:
-            redudant_cabels += network.union(u,v)
+            
+            graph[u].append(v)
+            graph[v].append(u)
         
-        cabels_needed = network.total_parents()
+        for i in range(n):
+            
+            if i not in visited:
+                
+                components += 1
+                self.dfs(i,graph,visited)
         
-        return -1 if cabels_needed > redudant_cabels else cabels_needed
+        return components - 1
+        
         
         
         
