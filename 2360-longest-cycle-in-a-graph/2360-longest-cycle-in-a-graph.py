eaf0ffe7c8119388a8efad862c31f1e427ad402a
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-03-26 12:54:58 +0300

    Time: 1350 ms (66.98%), Space: 122.6 MB (57.94%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,41 +1,44 @@
 class Solution:
     
-    def maxCycle(self,node,edges,indegree):
+    def findCycleSize(self,node,edges,in_degree):
         
-        cycle = 0
-        while(indegree[node] > 0):
-            
-            cycle += 1
-            indegree[node] -= 1
-            node = edges[node]
-            
-        return cycle
+        if in_degree[node] == 0:
+            return 0
         
+        in_degree[node] -= 1
+        
+        return 1 + self.findCycleSize(edges[node],edges,in_degree)
     
     def longestCycle(self, edges: List[int]) -> int:
         
-        indegree = [0 for _ in range(len(edges))]
-        answer = - 1
-        q = deque()
+        in_degree = [0 for _ in range(len(edges))]
+        queue = deque()
         
         for i in range(len(edges)):
-            if edges[i] != -1:
-                indegree[edges[i]] += 1
-        
-        for i in range(len(edges)):
-            if indegree[i] == 0:
-                q.append(i)
-                
-        while(q):
             
-            node = q.popleft()
-            if edges[node] != -1:
-                indegree[edges[node]] -= 1
-            if edges[node] != -1 and indegree[edges[node]] == 0:
-                q.append(edges[node])
+            if edges[i] != -1:
+                in_degree[edges[i]] += 1
                 
         for i in range(len(edges)):
-            if indegree[i] != 0:
-                answer = max(answer,self.maxCycle(i,edges,indegree))
+            if in_degree[i] == 0:
+                queue.append(i)
+                
+        while queue:
+            
+            node = queue.popleft()
+            
+            if edges[node] != -1:
+                in_degree[edges[node]] -= 1
+                
+            if edges[node] != - 1 and in_degree[edges[node]] == 0:
+                queue.append(edges[node])
+                
+        max_cycle = -1
         
-        return answer
+        for i in range(len(edges)):
+            
+            if in_degree[i] != 0:
+                max_cycle = max(max_cycle,self.findCycleSize(i,edges,in_degree))
+        
+        return max_cycle
+        
