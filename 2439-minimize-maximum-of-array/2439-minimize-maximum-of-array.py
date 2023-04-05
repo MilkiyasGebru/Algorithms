0e31feb113e3d4f29b3a1d9d82a57723d9c059d6
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-05 18:10:59 +0300

    Time: 790 ms (82.96%), Space: 26.9 MB (39.46%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -3,7 +3,9 @@ class Solution:
         
         
         max_ = summ = 0
+        
         for i in range(len(nums)):
+            
             summ += nums[i]
             max_ = max(max_,ceil(summ/(i+1) ))
         
