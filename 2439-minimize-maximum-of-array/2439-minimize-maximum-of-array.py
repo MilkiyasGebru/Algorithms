0464805184d01d650d83ff7188ec2fec1f5af0ca
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-05 18:08:38 +0300

    Time: 800 ms (78.03%), Space: 26.8 MB (65.02%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        
+        
+        max_ = summ = 0
+        for i in range(len(nums)):
+            summ += nums[i]
+            max_ = max(max_,ceil(summ/(i+1) ))
+        
+        return max_
+            
+            
+        
+        
