258ce542f51f6b1e0a19096319e54937c22ee6cf
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-06 11:18:22 +0300

    Time: 135 ms (45.64%), Space: 14.3 MB (93.84%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,45 +1,44 @@
 class Solution:
     
-    def isValid(self,row,col,visited,grid):
+    def isValid(self,row,col,grid):
         
-        return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == 0 and (row,col) not in visited
+        return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == 0 
     
-    def dfs(self,row,col,grid,visited):
+    def dfs(self,row,col,grid):
         
-        visited.add((row,col))
+        grid[row][col] = 1
         directions = [(-1,0),(1,0),(0,1),(0,-1)]
         
         for dx,dy in directions:
             new_row = dx + row
             new_col = dy + col
             
-            if self.isValid(new_row,new_col,visited,grid):
-                self.dfs(new_row,new_col,grid,visited)
+            if self.isValid(new_row,new_col,grid):
+                self.dfs(new_row,new_col,grid)
          
     def closedIsland(self, grid: List[List[int]]) -> int:
         
-        visited = set()
         answer = 0
         
         for row in range(len(grid)):
             for col in range(0,len(grid[0]),max(1,len(grid[0])-1)):
                 
                 if grid[row][col] == 0:
-                    self.dfs(row,col,grid,visited)
+                    self.dfs(row,col,grid)
         
         for col in range(len(grid[0])):
             for row in range(0,len(grid),max(len(grid)-1,1)):
                 
                 if grid[row][col] == 0:
-                    self.dfs(row,col,grid,visited)
+                    self.dfs(row,col,grid)
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 
-                if grid[row][col] == 0 and (row,col) not in visited:
+                if grid[row][col] == 0 :
                     
                     answer += 1
-                    self.dfs(row,col,grid,visited)
+                    self.dfs(row,col,grid)
         
         return answer 
                     
