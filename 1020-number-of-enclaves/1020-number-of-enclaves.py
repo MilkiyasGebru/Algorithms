75d656b4073f325beff17259dd4524d9358f47d5
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-07 12:13:44 +0300

    Time: 878 ms (24.93%), Space: 89.2 MB (19.53%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -5,6 +5,7 @@ class Solution:
         return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == 1
     
     def dfs(self,row,col,grid):
+        
         ans = 1
         grid[row][col] = 0
         directions = [(-1,0),(1,0),(0,-1),(0,1)]
@@ -16,23 +17,29 @@ class Solution:
             
             if self.isValid(new_row,new_col,grid):
                 ans += self.dfs(new_row,new_col,grid)
+                
         return ans
         
     
     def numEnclaves(self, grid: List[List[int]]) -> int:
+        
         enclaves = 0
+        
         for row in range(len(grid)):
             for col in range(0,len(grid[0]),max(len(grid[0])-1,1)):
+                
                 if grid[row][col] == 1:
                     self.dfs(row,col,grid)
         
         for col in range(len(grid[0])):
             for row in range(0,len(grid),max(len(grid)-1,1)):
+                
                 if grid[row][col] == 1:
                     self.dfs(row,col,grid)
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
+                
                 if grid[row][col] == 1:
                     enclaves += self.dfs(row,col,grid)
                     
