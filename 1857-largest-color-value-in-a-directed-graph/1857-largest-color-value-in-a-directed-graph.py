899ee1a05e8009226b915231366ffbdfa817c047
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-10 10:55:28 +0300

    Time: 4002 ms (6.06%), Space: 311.4 MB (5.45%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,49 @@
+class Solution:
+        
+    def findMax(self,f,g):
+        
+        for i in range(26):
+            f[i] = max(f[i],g[i])
+        
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        
+    
+        
+        visited = [0 for i in range(len(colors))]
+        graph = defaultdict(list)
+        answer = 0
+        
+        for u,v in edges:
+            
+            graph[u].append(v)
+        
+        @lru_cache(None)
+        def dfs(node):
+            
+            if visited[node] == 1:
+                return defaultdict(lambda : inf)
+            
+            if visited[node] == 2:
+                return f[node]
+            
+            f = defaultdict(int)
+            visited[node] = 1
+            
+            for child in graph[node]:
+                
+                self.findMax(f,dfs(child))
+                
+            visited[node] = 2
+                
+                
+            f[ord(colors[node])-ord("a")] += 1
+            return f
+        
+        for i in range(len(colors)):
+            if visited[i] == 0:
+                answer = max(max(dfs(i).values()),answer)
+        
+        return answer if answer != math.inf else -1
+            
+        
+        
