5d9d716a1986cd495eb1c0f790b0d50a2c6be8fd
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-11 10:15:38 +0300

    Time: 45 ms (63.36%), Space: 16.1 MB (14.66%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,18 +1,12 @@
 class Solution:
+    @lru_cache(None)
     def minDays(self, n: int) -> int:
-        visited = set()
-        heap = [(0,n)]
-        while heap:
-            
-            days,num = heapq.heappop(heap)
-            if num == 1:
-                return days + 1
-            if num in visited:
-                continue
-            visited.add(num)
-            
-            heapq.heappush(heap,(days + 1 + num%2,num//2))
-            heapq.heappush(heap,(days + 1 + num%3,num//3))
+        
+        if n <= 1:
+            return n
+        
+        return 1 + min(self.minDays(n//2) + n%2 , self.minDays(n//3) + n%3 )
+        
         
             
             
