fbe6df8a0562fd1c0b49e6e2111f89ce84685df5
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-12 08:41:30 +0300

    Time: 42 ms (17.20%), Space: 13.9 MB (28.25%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,13 +1,20 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack=[]
-        for char in path.split('/'):
-            if char==""or char==".":
+        
+        path = path.split('/')
+        stack = []
+        
+        for file in path:
+            
+            if file == "" or file == "." :
                 continue
-            elif char=='..':
+                
+            elif  file == "..":
+                
                 if stack:
                     stack.pop()
+                    
             else:
-                stack.append(char)
-        return "/"+ "/".join(stack)
-        
+                stack.append(file)
+                
+        return "/" + '/'.join(stack)
