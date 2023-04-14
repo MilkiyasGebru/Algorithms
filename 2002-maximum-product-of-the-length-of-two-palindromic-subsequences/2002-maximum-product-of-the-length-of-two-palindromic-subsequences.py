a53344aecd54446d4d99591d96e8deda0398a375
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-14 11:34:21 +0300

    Time: 3772 ms (16.58%), Space: 739.4 MB (5.10%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -1,39 +1,25 @@
 class Solution:
     def maxProduct(self, s: str) -> int:
-        def isPalindrom(path):
+        def isPalindrom(s):
             
-            arr = []
-            for i in range(len(path)):
-                if path[i] == '1':
-                    arr.append(s[i])
-            
-            string = "".join(arr)
-            return (string != "") and  string == string[::-1]
-        possible = []
-        def backTrack(index,path):
+            if s != "" and s == s[::-1]:
+                return len(s)
+            return 0
+        
+        @lru_cache(None)
+        def dp(index,s1,s2):
             
             if index == len(s):
-                if isPalindrom(path):
-                    possible.append((int(path,2),path.count("1")))
-                return
-            backTrack(index+1 , path +"1")
-            backTrack(index+1, path +"0")
-        answer = 0
-        backTrack(0,"")
+                
+                return isPalindrom(s1)*isPalindrom(s2)
+            
+            return max(
+                dp(index+1,s1+s[index],s2),
+                dp(index+1,s1,s2+s[index]),
+                dp(index+1,s1,s2)
+            )
         
-        for i in range(len(possible)):
+        return dp(0,"","")
             
-            x = possible[i][0]
-            count_x = possible[i][1]
-            
-            for j in range(i+1,len(possible)):
-                
-                y = possible[j][0]
-                count_y = possible[j][1]
-                
-                if x & y == 0:
-                    answer = max(count_y * count_x,answer)
-                    
-        return answer
         
         
