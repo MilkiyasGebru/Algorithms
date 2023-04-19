0802058c073aaa59106a73ade727a7f150d513e8
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-19 07:58:50 +0300

    Time: 48 ms (57.48%), Space: 14.2 MB (26.15%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        @lru_cache(None)
+        def dp(i,j):
+            
+            if j == len(p):
+                return i == len(s)
+            
+            first = i < len(s) and p[j] in (".",s[i])
+            second = j+1 < len(p) and p[j+1] == "*"
+            
+            return (first and dp(i+1,j+1)) or (first and second and (dp(i+1,j) or dp(i+1,j+2) or dp(i,j+2))) or (second and dp(i,j+2))
+        
+        return dp(0,0)
