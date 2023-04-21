d56adbd763d96fea4cff30683b9d4be0217f1e16
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-04-21 10:55:43 +0300

    Time: 4793 ms (6.66%), Space: 665.1 MB (7.62%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,7 +1,7 @@
 class Solution:
     
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        
+        mod = 10**9 + 7
         @lru_cache(None)
         def dp(i,n,min_profit):
             
@@ -11,6 +11,6 @@ class Solution:
             if i == len(group) or n == 0:
                 return 1 if min_profit == minProfit else 0
             
-            return dp(i+1,n-group[i],min(minProfit,min_profit+profit[i])) + dp(i+1,n,min_profit)
+            return (dp(i+1,n-group[i],min(minProfit,min_profit+profit[i])))%mod + (dp(i+1,n,min_profit))%mod
         
-        return dp(0,n,0)%(10**9+7)
+        return dp(0,n,0)%mod
