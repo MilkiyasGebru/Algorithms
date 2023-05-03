fd5dbf0dbd16168a3c8e81298027e39f79a68516
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-05-03 12:47:04 +0300

    Time: 97 ms (86.22%), Space: 18.9 MB (15.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,11 +3,13 @@ class Solution:
         
         @lru_cache(None)
         def dp(i,j):
+            
             if i == len(word1) or j == len(word2):
-                return max(len(word1)-i,len(word2) - j)
+                return max(len(word1)-i, len(word2)-j)
+            
             if word1[i] == word2[j]:
                 return dp(i+1,j+1)
             
-            return min(1 + dp(i,j+1), 1 + dp(i+1,j+1), 1+ dp(i+1,j) )
+            return min(1 + dp(i,j+1), 1 + dp(i+1,j+1), 1+ dp(i+1,j))
         
         return dp(0,0)
