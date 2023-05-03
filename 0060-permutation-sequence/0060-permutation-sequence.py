b95ad01c188b8f9fb298629db375231355ad859c
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-05-03 11:20:34 +0300

    Time: 56 ms (25.44%), Space: 16.5 MB (16.01%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -2,25 +2,38 @@ class Solution:
     
     @lru_cache(None)
     def factorial(self,n):
-        
         if n <= 1:
             return 1
         
         return n*self.factorial(n-1)
     
+    def findMax(self,curr,n):
+        total = 0
+        for i in range(curr,n+1):
+            remain = n-i
+            total += remain*self.factorial(remain)
+        return total
+    
     def getPermutation(self, n: int, k: int) -> str:
         
-        arr = [i for i in range(1,n+1)]
-        num = ""
-        k-=1
+        self.answer = None
+        def backTrack(i,count,arr):
+            
+            if count > k or self.answer or count + self.findMax(i,n) < k  :
+                return self.answer
+            
+            if i == n+1:
+                self.answer = "".join(arr)  
+                return self.answer
+            
+            less = 0
+            
+            for num in range(1,n+1):
+                
+                if str(num) not in arr:
+                    remain = n - i
+                    backTrack(i+1,count + less*self.factorial(remain),arr+[str(num)])
+                    less += 1
         
-        for i in range(n-1,-1,-1):
-            
-            div = k // self.factorial(i)
-            k -= div * self.factorial(i)
-            num += str(arr[div])
-            arr.remove(arr[div])
-        
-        return num
-            
-            
+        backTrack(1,1,[])
+        return self.answer
