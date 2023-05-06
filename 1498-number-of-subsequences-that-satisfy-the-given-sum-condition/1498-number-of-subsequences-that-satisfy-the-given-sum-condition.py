5b8e9001ff43f3049cfdec6162f8309ba7e69946
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-05-06 11:04:48 +0300

    Time: 802 ms (88.62%), Space: 28.5 MB (15.98%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,26 +1,6 @@
 class Solution:
     
-    @lru_cache(None)
-    def possible(self,num):
-        if num == 0:
-            return 0
-        return 1 + 2*possible(num-1)
-    
-    def binary_search(binary_search,nums,val,right):
-        
-        left = 0
-        # right = len(nums)
-        
-        while left <= right:
-            
-            mid = (left + right)//2
-            if nums[mid] <= val:
-                left = mid + 1
-            else:
-                right = mid
-        
-        return left 
-    
+     
     def numSubseq(self, nums: List[int], target: int) -> int:
         
         nums.sort()
