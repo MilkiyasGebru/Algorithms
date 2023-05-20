54daf73110aa5e85e24d80407eaae504af75759e
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-05-20 09:12:33 +0300

    Time: 47 ms (24.68%), Space: 16.3 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,36 +1,33 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # build adjecency list
-        adj_list = {}
+    
+    def calculate(self,node,end,visited,graph):
         
-        for (eq, val) in zip(equations, values):
-            if eq[0] not in adj_list:
-                adj_list[eq[0]] = []
-            adj_list[eq[0]].append((eq[1], val))
-
-            if eq[1] not in adj_list:
-                adj_list[eq[1]] = []
-            adj_list[eq[1]].append((eq[0], 1 / val))
-
-        n = len(queries)
-        ans = [-1] * n
-
-        for i in range(n):
-            if queries[i][0] not in adj_list:
-                continue
-            visited = set()
-            que = deque()
-            que.append((queries[i][0], 1))
-
-            while que:
-                k, v = que.popleft()
-                if k == queries[i][1]:
-                    ans[i] = v
-                    break
-                if k in visited:
-                    continue
-                visited.add(k)
-                for b1, b2 in adj_list[k]:
-                    que.append((b1, b2 * v))
-
-        return ans
+        if node in visited or graph[node] == [] or graph[end] == []:
+            return -math.inf
+        
+        if node == end:
+            return 1
+        
+        visited.add(node)
+        val = -math.inf
+        
+        for child,wei in graph[node]:
+            val = max(val, wei*self.calculate(child,end,visited,graph))
+        return val
+    
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+        graph = defaultdict(list)
+        
+        for i in range(len(equations)):
+            
+            graph[equations[i][0]].append((equations[i][1],values[i]))
+            graph[equations[i][1]].append((equations[i][0],1/values[i]))
+        
+        answer = []
+        for u,v in queries:
+            answer.append(max(-1.0,self.calculate(u,v,set(),graph)))
+        
+        return answer
+        
+        
