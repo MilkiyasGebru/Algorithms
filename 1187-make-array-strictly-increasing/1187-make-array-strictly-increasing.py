6fc91f809af20a6887d0e57bc4087ccba477a604
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-07-03 14:05:39 +0300

    Time: 1791 ms (8.43%), Space: 158.2 MB (5.29%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.py
@@ -13,28 +13,23 @@ class Solution:
             
     def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
         arr2.sort()
-        arr1= [-math.inf ] + arr1
-        f = {0:arr1,1:arr2}
         
         @cache
-        def dp(i,prev_max,array):
+        def dp(i,prev_max):
             
             if i == len(arr1):
                 return 0
-            
             minn = math.inf
+            if arr1[i] > prev_max:
+                minn = dp(i+1,arr1[i])
             
-            if arr1[i] > f[array][prev_max]:
-                minn = dp(i+1,i,0)
-            
-            next_index = self.binarySearch(arr2,f[array][prev_max])
-            
-            if next_index < len(arr2) and arr2[next_index] > f[array][prev_max] :
-                minn = min(1 + dp(i+1,next_index,1),minn)
+            idx = self.binarySearch(arr2,prev_max)
+            if idx < len(arr2):
+                minn = min(1+dp(i+1,arr2[idx]),minn)
             
             return minn
             
-        return dp(1,0,0) if dp(1,0,0) != math.inf else -1
+        return dp(0,-math.inf) if dp(0,-math.inf) != math.inf else -1
         
 
             
