e98bfe656ac64f089789b4471f272c653a791b3a
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-07-04 09:19:20 +0300

    Time: 5957 ms (5.01%), Space: 102.2 MB (5.41%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -3,15 +3,14 @@ class Solution:
         
         
         dp = defaultdict(lambda : 1)
-        dp[(0,0)] = 1
+        
         for i in range(len(nums)-1,-1,-1):
             for j in range(i+1,len(nums)):
                 diff = nums[j]-nums[i]
-                if diff != 0:
                     
-                    dp[(nums[i],diff)] = max(dp[(nums[i],diff)], 1 + dp[(nums[j],diff)])
+                dp[(i,diff)] = max(dp[(i,diff)], 1 + dp[(j,diff)])
         
         
-        return max(max(dp.values()),max(Counter(nums).values()))
+        return max(dp.values())
         
             
