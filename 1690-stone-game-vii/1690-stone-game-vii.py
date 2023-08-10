e5db5c3b4657996096a8156c3392a1aa49b4057d
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-08-10 09:22:08 +0300

    Time: 2323 ms (99.35%), Space: 40.9 MB (38.31%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.py
+++ 1690-stone-game-vii/1690-stone-game-vii.py
@@ -7,15 +7,15 @@ class Solution:
             prefix.append(prefix[-1] + stone)
         
         dp =[[0 for _ in range(len(stones)+1)]for _ in range(len(stones)+1)]
+        
         for left in range(len(stones),-1,-1):
-            for right in range(len(stones)+1):
-                if left >= right:
-                    dp[left][right] = 0
-                else:
-                    dp[left][right] = max(
-                     prefix[right]-prefix[left] - dp[left+1][right],
-                        
-                prefix[right-1] - prefix[left-1] - dp[left][right-1])
+            
+            for right in range(left+1,len(stones)+1):
+                
+                dp[left][right] = max(
+                 prefix[right]-prefix[left] - dp[left+1][right],
+
+            prefix[right-1] - prefix[left-1] - dp[left][right-1])
 
         
         return dp[1][-1]
