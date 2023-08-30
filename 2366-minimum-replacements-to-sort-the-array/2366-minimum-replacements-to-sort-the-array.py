a681440a4cf42e5d7d2fcb91f041f2bb72f0d6ae
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-08-30 10:10:05 +0300

    Time: 506 ms (55.37%), Space: 31.3 MB (45.45%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -3,13 +3,17 @@ class Solution:
         
         maxx = nums[-1]
         operation = 0
+        
         for i in range(len(nums)-2,-1,-1):
             
             if nums[i] > maxx:
                 
                 operation += math.ceil(nums[i]/maxx) - 1
                 div = math.ceil(nums[i]/maxx)
-                maxx = maxx if nums[i]%maxx == 0 else math.floor(nums[i]/div)
+                maxx =  math.floor(nums[i]/div)
+                
             else:
+                
                 maxx = nums[i]
+                
         return operation
