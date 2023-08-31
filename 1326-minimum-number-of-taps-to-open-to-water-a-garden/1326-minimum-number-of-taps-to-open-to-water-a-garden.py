af79cda762675936e9cf3d520344abefdfeedb15
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-08-31 10:40:06 +0300

    Time: 145 ms (65.95%), Space: 18.6 MB (17.86%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,26 +1,40 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        intervals = []
-        for i in range(n + 1):
-            left = max(0, i - ranges[i])
-            right = min(n, i + ranges[i])
-            intervals.append((left, right))
-
-        intervals.sort(key=lambda x: x[0])
-        curr_end = 0
-        next_end = 0
-        num_taps = 0
+        
+        arr = []
+        
+        for i in range(len(ranges)):
+            
+            arr.append((max(0,i-ranges[i]),min(len(ranges)-1,i+ranges[i])))
+        
+        arr.sort(key = lambda x: x[0])
+        last = 0
+        tapsNeeded = 0
+        
         i = 0
-
-        while i <= n and curr_end < n:
-            while i <= n and intervals[i][0] <= curr_end:
-                next_end = max(next_end, intervals[i][1])
-                i += 1
-
-            if curr_end == next_end:
+        while i < len(arr) and last < len(ranges)-1:
+            
+            temp = last
+            
+            while i < len(arr) and arr[i][0] <= last :
+                temp = max(arr[i][1],temp)
+                i+=1
+                
+            if last == temp:
                 return -1
-
-            num_taps += 1
-            curr_end = next_end
-
-        return num_taps
+            
+            last = temp
+            tapsNeeded += 1 
+       
+        return tapsNeeded 
+            
+            
+            
+            
+        
+        
+            
+        
+        
+        
+        
