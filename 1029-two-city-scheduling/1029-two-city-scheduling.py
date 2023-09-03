e3094a0f42b84ac6b928c634996f89dddbf9a9ac
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-09-03 09:49:53 +0300

    Time: 49 ms (63.68%), Space: 16.4 MB (22.00%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,14 +1,21 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        @cache
-        def dp(index,a,b):
-            if a > len(costs)//2 or b > len(costs)//2:
-                return float("inf")
-            if index == len(costs):
-                return 0
-            
-            return min(costs[index][0] + dp(index+1,a+1,b), costs[index][1] + dp(index+1,a,b+1))
         
-        return dp(0,0,0)
+        '''Greedy Approach'''
+        
+        
+        costs.sort(key = lambda x : abs(x[1]-x[0]), reverse=True)
+        min_cost,count1,count2 = 0,0,0
+        for i in range(len(costs)):
+            
+            if (count1 < len(costs)//2 and costs[i][0] <= costs[i][1]) or count2 == len(costs)//2:
+                count1 += 1
+                min_cost += costs[i][0]
+            else:
+                min_cost += costs[i][1]
+                count2 += 1
+        
+        return min_cost
+        
             
             
