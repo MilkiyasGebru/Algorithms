c26897d2a20116651592d6a412a12ce9168c01b0
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-09-05 11:41:26 +0300

    Time: 769 ms (75.00%), Space: 28.7 MB (70.19%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.py
@@ -1,19 +1,19 @@
 class Solution:
     def shortestSequence(self, rolls: List[int], k: int) -> int:
-        visited = set()
-        def addtoSet():
-            for i in range(1,k+1):
-                visited.add(i)
-        addtoSet()
-        count = 1
-        for i in range(len(rolls)-1,-1,-1):
-            
-            if rolls[i] in visited:
-                visited.remove(rolls[i])
-            
-            if len(visited) == 0:
-                count += 1
-                addtoSet()
         
+        visited = set()
+        
+        count = 1
+        
+        for i in range(len(rolls)):
+            
+            visited.add(rolls[i])
+            
+            if len(visited) == k:
+                
+                visited = set()
+                count += 1    
+            
+            
         return count 
         
