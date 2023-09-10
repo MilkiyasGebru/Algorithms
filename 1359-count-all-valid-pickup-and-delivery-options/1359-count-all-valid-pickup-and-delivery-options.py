62e9d8d4d55d4c72d808ee7c413f1a8ee3a449dd
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-09-10 16:25:41 +0300

    Time: 44 ms (45.96%), Space: 16.5 MB (23.40%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,12 +1,8 @@
 class Solution:
     def countOrders(self, n: int) -> int:
         mod = 10**9 + 7
-        def rec(n):
-            
-            if n == 1:
-                return 1
-            
-            return ((2*n-1)*n * rec(n-1))%mod
+        if n == 1:
+            return 1
+        return ((2*n-1)*n*self.countOrders(n-1))%mod
         
-        return rec(n)
             
