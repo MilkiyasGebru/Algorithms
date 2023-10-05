e83f3955ae282bfb2a1c57f3d328f859e09a0682
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-10-05 15:13:03 +0300

    Time: 597 ms (64.95%), Space: 28.3 MB (66.33%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,23 +1,31 @@
+# from collections import heapq
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         
-        heap = [[-1,start]]
-        visited = set()
         graph = defaultdict(list)
         
         for i in range(len(edges)):
-            graph[edges[i][0]].append([edges[i][1],succProb[i]])
-            graph[edges[i][1]].append([edges[i][0],succProb[i]])
+            
+            graph[edges[i][0]].append((succProb[i],edges[i][1]))
+            graph[edges[i][1]].append((succProb[i],edges[i][0]))
         
-        while heap :
-            weight,node = heappop(heap)
+        heap = []
+        heapq.heappush(heap,(-1,start_node))
+        visited = set()
+        
+        while heap:
+            
+            weight,node = heapq.heappop(heap)
+            
             if node in visited:
                 continue
-            if node == end:
+                
+            if node == end_node:
                 return -1*weight
+                
             visited.add(node)
-            for neighbour,w in graph[node]:
-                heappush(heap,(-1*abs(w*weight),neighbour))
+            for wx,neigbour in graph[node]:
+                heapq.heappush(heap,(wx*weight, neigbour))
         
         return 0
             
