5440d8e1015c24787f9bc25b063b46429089c141
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2023-10-12 16:14:34 +0300

    Time: 45 ms (15.66%), Space: 16.9 MB (98.91%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -6,60 +6,57 @@
 #    def get(self, index: int) -> int:
 #    def length(self) -> int:
 
-
 class Solution:
-    def binary_search_increasing(self,mountain_arr,left,right,target):
-        
-        while left < right:
-            
-            mid = (left + right)//2
-            value = mountain_arr.get(mid)
-            
-            if value >= target:
-                right = mid
-            else:
-                left = mid + 1
-        return math.inf if left == mountain_arr.length() or mountain_arr.get(left) != target else left
-    def binary_search_decreasing(self,mountain_arr,left,right,target):
-        
-        while left < right:
-            
-            mid = (left + right)//2
-            
-            if mountain_arr.get(mid)  <= target:
-                right = mid
-            else:
-                left = mid + 1
-        
-        return math.inf if left == mountain_arr.length() or mountain_arr.get(left) != target else left   
-        
-    
-        
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        '''
-        I am Given A moutain Array... which is a two sorted array joined. one in descending 
-        the other ascending.
-
-        I am thinking of binary search but how would I find the pivot point???
-
-        '''
         
-        #Lets first find the peak element
         
-        left = 1
-        right = mountain_arr.length()-1
-        while left < right:
+        def find_pivot():
             
-            mid = (left + right) // 2
-            val1,val2,val3 = mountain_arr.get(mid-1),mountain_arr.get(mid),mountain_arr.get(mid+1)
+            left = 1
+            right = mountain_arr.length() - 1
             
-            if val1 < val2 > val3:
-                left = mid 
-                break
-            elif val1 < val2 < val3:
-                left = mid + 1
-            else:
-                right = mid 
-        possible_index = min(self.binary_search_increasing(mountain_arr,0,left+1,target),
-                            self.binary_search_decreasing(mountain_arr,left,mountain_arr.length(),target))
-        return possible_index if possible_index != math.inf else -1
+            while left < right:
+                
+                mid = (left + right) // 2
+                
+                if mountain_arr.get(mid) > mountain_arr.get(mid+1):
+                    
+                    right = mid
+                else:
+                    left = mid + 1
+            
+            return left
+        
+        
+        def find_increasing_array():
+            left = 0
+            right = pivot_index + 1
+            
+            while left < right:
+                
+                mid = (left + right)//2
+                
+                if mountain_arr.get(mid) >= target:
+                    right = mid
+                else:
+                    left = mid + 1
+            
+            return math.inf if mountain_arr.get(left) != target else left
+        
+        def find_decreasing_array():
+            left = pivot_index
+            right = mountain_arr.length()
+            
+            while left < right:
+                mid = (left + right)//2
+                if mountain_arr.get(mid) <= target:
+                    right = mid
+                else:
+                    left = mid + 1
+            return math.inf if left >= mountain_arr.length() or target != mountain_arr.get(left) else left
+        pivot_index = find_pivot()
+        min_index = min(find_increasing_array(),find_decreasing_array())
+        return min_index if min_index != math.inf else -1
+            
+                
+                
