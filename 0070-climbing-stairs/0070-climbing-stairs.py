d4fcefc12ed8fb9278c4a2f44ed1b4365080e0d0
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-01-18 21:35:51 +0300

    Time: 31 ms (87.49%), Space: 17.4 MB (15.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        @cache
+        def rec(n):
+            
+            if n == 0:
+                return 1
+            if n < 0:
+                return 0
+            
+            return rec(n-1) + rec(n-2)
+        
+        return rec(n)
