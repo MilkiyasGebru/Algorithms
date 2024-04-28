6aec06ba3dab96eeb0f8f2b79e7128df0ad9216f
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-04-28 09:26:54 +0300

    Time: 142 ms (48.06%), Space: 17.2 MB (45.58%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        
+        f = defaultdict(list)
+        @cache
+        def dp(curr_index,key_index):
+            
+            if key_index >= len(key):
+                return 0
+            
+            if ring[curr_index] == key[key_index]:
+                return 1 + dp(curr_index, key_index + 1)
+            
+            left_most = right_most = curr_index
+            left_cost = right_cost = 0
+            while ring[left_most] != key[key_index]:
+                left_cost += 1
+                left_most -= 1
+                left_most %= len(ring)
+                
+            while ring[right_most] != key[key_index]:
+                right_cost += 1
+                right_most += 1
+                right_most %= len(ring)
+            
+            return 1 + min(left_cost + dp(left_most,key_index+1),right_cost + dp(right_most,key_index+1))
+        
+        return dp(0,0)
