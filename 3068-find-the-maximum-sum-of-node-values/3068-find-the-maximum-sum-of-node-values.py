7e1f9081788359ef0c51d177796b82255b4d6723
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-05-20 19:48:42 +0300

    Time: 998 ms (33.33%), Space: 28.2 MB (52.38%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,19 +1,16 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
         
-        new_array = []
-        for i in range(len(nums)):
-            new_array.append(((nums[i]^k)-nums[i],nums[i]))
+        new_array = [((nums[i]^k)-nums[i],nums[i]) for i in range(len(nums))]
         new_array.sort(reverse=True)
-        i = total = 0
-        while i < len(new_array) and new_array[i][0] >= 0: 
-            
-            if i+1 < len(new_array) and new_array[i][0] + new_array[i+1][0] >= 0:
-                total += new_array[i][0] + new_array[i+1][0] + new_array[i][1] + new_array[i+1][1]
-            else:
-                break
-            i += 2
         
+        i = total = 0
+        
+        while i+1 < len(new_array) and new_array[i][0] + new_array[i+1][0] >= 0: 
+            
+            total += new_array[i][0] + new_array[i+1][0] + new_array[i][1] + new_array[i+1][1]
+            i += 2
+
         for j in range(i,len(new_array)):
             total += new_array[j][1]
         
