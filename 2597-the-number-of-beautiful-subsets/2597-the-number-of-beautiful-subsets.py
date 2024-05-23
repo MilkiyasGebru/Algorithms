cac3af0e6531c67cfd43135aa668fb8c03b99717
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-05-23 12:02:09 +0300

    Time: 2467 ms (67.62%), Space: 16.3 MB (98.31%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        
+        
+        
+        def backTrack(index,f):
+            if index == len(nums):
+                return 1
+            
+            total = backTrack(index+1,f)
+            
+            if f[nums[index]+k] == 0 and f[nums[index]-k] == 0:
+                f[nums[index]] += 1
+                total += backTrack(index+1,f)
+                f[nums[index]] -= 1
+            return total
+        
+        return backTrack(0,defaultdict(int))-1
