41005041c8e6b786643c0d55c407deaf3d368ed1
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-11-15 09:06:31 +0300

    Time: 1294 ms (5.41%), Space: 40.3 MB (6.23%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,32 +1,39 @@
 class Solution:
-    def binary_search(self,left,val, arr):
+    
+    def isValid(self,arr,length):
+        decreasing_arr = [(math.inf,0)]
+        for i in range(len(arr)-1,-1,-1):
+            # while decreasing_arr and decreasing_arr[-1][0] < arr[i]:
+            #     decreasing_arr.pop()
+            decreasing_arr.append((arr[i],decreasing_arr[-1][1]+ (1 if decreasing_arr[-1][0] >= arr[i] else 0)))
+        
+        decreasing_arr.reverse()
+        increasing_arr = [(0,0)]
+        for i in range(len(arr)):
+            right_index = i + length
+            if right_index >= len(decreasing_arr):
+                return False
+            if increasing_arr[-1][0] <= decreasing_arr[right_index][0] and increasing_arr[-1][1] + decreasing_arr[right_index][1] == len(arr) - length:
+                return True
+            
+            while increasing_arr and increasing_arr[-1][0] > arr[i]:
+                increasing_arr.pop()
+            increasing_arr.append((arr[i],increasing_arr[-1][1]+1))
+        return False
+    
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        
+        left = 0
         right = len(arr)
         while left < right:
+            
             mid = (left + right)//2
             
-            if arr[mid] >= val:
+            if self.isValid(arr,mid):
                 right = mid
             else:
                 left = mid + 1
-        return left
-    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        
-        left = len(arr)-1
-        for i in range(len(arr)-2,-1,-1):
-            if arr[i] <= arr[i+1]:
-                left = i
-            else:
-                break
-        
-        prev_max = -math.inf
-        answer = len(arr)
-        for i in range(len(arr)):
-            
-            answer = min(self.binary_search(max(left,i),prev_max,arr)-i,answer)
-            if prev_max > arr[i]:
-                break
-            prev_max = max(arr[i],prev_max)
-        return answer
+        return right
             
         
         
