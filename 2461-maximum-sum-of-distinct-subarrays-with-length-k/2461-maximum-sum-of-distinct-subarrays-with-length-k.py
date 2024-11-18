d67e21a84f52825cda4450b94aea9ea4d9d16520
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-11-18 13:50:52 +0300

    Time: 184 ms (13.52%), Space: 36.6 MB (9.19%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -7,15 +7,13 @@ class Solution:
             freq[nums[right]] += 1
             total += nums[right]
             
-            while freq[nums[right]] > 1:
+            while freq[nums[right]] > 1 or right-left + 1 > k:
                 freq[nums[left]] -= 1
                 total -= nums[left]
                 left += 1
             
             if right - left + 1 == k:
                 answer = max(answer,total)
-                total -= nums[left]
-                freq[nums[left]] -= 1
-                left += 1
+                
         return answer
                 
