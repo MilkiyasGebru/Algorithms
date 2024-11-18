f2506b57881983280d3a50720b6af8807e89ff57
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-11-18 10:53:48 +0300

    Time: 0 ms (100.00%), Space: 16.7 MB (62.03%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -1,24 +1,18 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        
-        prefix = []
         answer = []
-        
-        if k < 0 :
-            code.reverse()
-            
-            
-        for j in range(2):
-            for i in range(len(code)):
-                if not prefix:
-                    prefix.append(code[i])
-                else:
-                    prefix.append(prefix[-1] + code[i])
+        if k > 0:
+            start,end = 1,k
+        else:
+            start,end = len(code)-abs(k),len(code)-1
+        window_sum = 0
+        for i in range(start,end+1):
+            window_sum += code[i]
         
         for i in range(len(code)):
-            
-            answer.append(prefix[i+abs(k)]-prefix[i])
-        if k < 0:
-            answer.reverse()
+            answer.append(window_sum)
+            window_sum -= code[(start)%len(code)]
+            window_sum += code[(end+1)%len(code)]
+            start += 1
+            end += 1
         return answer
-            
