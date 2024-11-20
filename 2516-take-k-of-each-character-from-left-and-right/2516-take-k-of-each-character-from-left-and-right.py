fe107b78739970a60ab588106b496c245f108461
MilkiyasGebru <95368135+MilkiyasGebru@users.noreply.github.com>
2024-11-20 09:56:09 +0300

    Time: 191 ms (76.67%), Space: 17.2 MB (55.20%) - LeetHub

--- 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
+++ 2516-take-k-of-each-character-from-left-and-right/2516-take-k-of-each-character-from-left-and-right.py
@@ -1,29 +1,19 @@
 class Solution:
-    
-    def isValid(self,s,length,k):
-        total_freq = Counter(s)
-        freq = Counter(s[:length])
-        for i in range(length,len(s)):
-            if total_freq['a'] - freq['a'] >= k and total_freq['b'] - freq['b'] >= k and total_freq['c'] - freq['c'] >= k:
-                return True
-            freq[s[i]] += 1
-            freq[s[i-length]] -= 1
-        return total_freq['a'] - freq['a'] >= k and total_freq['b'] - freq['b'] >= k and total_freq['c'] - freq['c'] >= k
-            
-    
     def takeCharacters(self, s: str, k: int) -> int:
+        total_freq = Counter(s)
+        if total_freq["a"] < k or total_freq["b"] < k or total_freq["c"] < k:
+            return -1
         
+        freq = defaultdict(int)
         left = 0
-        right = len(s) + 1
-        
-        while left < right:
+        max_length = 0
+        for right in range(len(s)):
             
-            mid = (left + right)//2
+            freq[s[right]] += 1
             
-            if self.isValid(s,mid,k):
-                left = mid + 1
-            else:
-                right = mid
-        return len(s) - (left-1) if left-1 >=0 else -1
-        
-        
+            while total_freq[s[right]] - freq[s[right]] < k:
+                
+                freq[s[left]] -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+        return len(s)-max_length
